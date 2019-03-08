93ea35b317b04f834b231efadc9e5401b6c4122b
Martin Odersky <odersky@gmail.com>
2019-03-08 15:23:02 +0100

    Rename impliedOnly in Import,ImortInfo to importImplied
    An import implied will also import old-style implicits, so the name is not
    correct anymore.

--- library/src/scala/tasty/reflect/Kernel.scala
+++ library/src/scala/tasty/reflect/Kernel.scala
@@ -193,13 +193,13 @@ trait Kernel {
 
   def matchImport(tree: Tree)(implicit ctx: Context): Option[Import]
 
-  def Import_impliedOnly(self: Import): Boolean
+  def Import_implied(self: Import): Boolean
   def Import_expr(self: Import)(implicit ctx: Context): Term
   def Import_selectors(self: Import)(implicit ctx: Context): List[ImportSelector]
 
-  def Import_apply(impliedOnly: Boolean, expr: Term, selectors: List[ImportSelector])(implicit ctx: Context): Import
+  def Import_apply(importImplied: Boolean, expr: Term, selectors: List[ImportSelector])(implicit ctx: Context): Import
 
-  def Import_copy(original: Import)(impliedOnly: Boolean, expr: Term, selectors: List[ImportSelector])(implicit ctx: Context): Import
+  def Import_copy(original: Import)(importImplied: Boolean, expr: Term, selectors: List[ImportSelector])(implicit ctx: Context): Import
 
   /** Tree representing a definition in the source code. It can be `PackageDef`, `ClassDef`, `TypeDef`, `DefDef` or `ValDef` */
   type Definition <: Statement
@@ -521,7 +521,7 @@ trait Kernel {
   //
   // CASES
   //
-  
+
   /** Branch of a pattern match or catch clause */
   type CaseDef <: AnyRef
 
@@ -544,7 +544,7 @@ trait Kernel {
   //
   // PATTERNS
   //
-  
+
   /** Pattern tree of the pattern part of a CaseDef */
   type Pattern <: AnyRef
 
@@ -609,7 +609,7 @@ trait Kernel {
   //
   // TYPE TREES
   //
-  
+
   /** Type tree representing a type or a bounds written in the source */
   type TypeOrBoundsTree <: AnyRef
 
