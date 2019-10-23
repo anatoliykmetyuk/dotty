6c462c350ff24c757e9fcaab3856144848b800a1
Anatolii <anatoliykmetyuk@gmail.com>
2019-10-23 15:39:33 +0200

    Entropy: more exception-related methods

--- compiler/src/dotty/entropy/SaneExceptions.scala
+++ compiler/src/dotty/entropy/SaneExceptions.scala
@@ -16,18 +16,23 @@ object SaneExceptions
     val prefix = jarNameToPrefix(jarName)
     s"$prefix$fileFolder"
 
+  def getStack(thr: Throwable): String =
+    val trace = thr.getStackTrace.map { t =>
+      val cn = t.getClassName.replace('.', '/') + ".class"
+      val classFile = this.getClass.getClassLoader.getResource(cn).toString
+
+      val scalaFile = s"${classFileToPackageSrc(classFile)}/${t.getFileName}"
+      val classNameShort = t.getClassName.reverse.takeWhile(_ != '.').reverse
+
+      s"\u001b[44;1m$classNameShort.${t.getMethodName}\u001b[0m $scalaFile:${t.getLineNumber}"
+    }.mkString("\n    ")
+    s"\u001b[48;5;88m${thr.getMessage}\u001b[0m\n    $trace"
+
+  def getStack(msg: String = "Stack trace"): String =
+    getStack(Exception(msg))
+
   def dumpStack(msg: String = "Stack trace"): Unit =
     SaneExceptions(throw Exception(msg))
 
   def apply(f: => Unit): Unit = try f catch
-    case thr =>
-      val trace = thr.getStackTrace.map { t =>
-        val cn = t.getClassName.replace('.', '/') + ".class"
-        val classFile = this.getClass.getClassLoader.getResource(cn).toString
-
-        val scalaFile = s"${classFileToPackageSrc(classFile)}/${t.getFileName}"
-        val classNameShort = t.getClassName.reverse.takeWhile(_ != '.').reverse
-
-        s"\u001b[44;1m$classNameShort.${t.getMethodName}\u001b[0m $scalaFile:${t.getLineNumber}"
-      }.mkString("\n    ")
-      println(s"\u001b[48;5;88m${thr.getMessage}\u001b[0m\n    $trace")
+    case thr => println(getStack(thr))
